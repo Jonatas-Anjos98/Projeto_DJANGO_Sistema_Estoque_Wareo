525b16d8a2611b552c3bbc0fd694da0d46a9658a
Jonatas <anjos92489@gmail.com>
2025-11-18 19:42:47 -0300

    update repository

--- app/settings.py
+++ app/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
 
     'products',
 ]
+GRAPPELLI_ADMIN_TITLE = 'SGP'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
